f11783c86dc2428ff4802efa7ceafe1549aca5be
lourival <lourival_silva@msn.com>
2023-05-30 20:48:43 -0300

    Subindo IP Allowed

--- plagos/settings.py
+++ plagos/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vs=yx=ai4&z+=m0!#yr3ik)7krn43%atzw7u=wlk8a^vr@-uo^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [18.222.20.202]
+ALLOWED_HOSTS = ['18.222.20.202']
 
 
 # Application definition
