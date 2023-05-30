8668d7c2fe090816e9412cd07d77d5da4619c894
lourival <lourival_silva@msn.com>
2023-05-30 02:33:34 -0300

    Commit da 1a. versão

--- plagos/settings.py
+++ plagos/settings.py
@@ -95,18 +95,24 @@ WSGI_APPLICATION = 'plagos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'plagos',
+ #       'USER': 'root',
+ #       'PASSWORD': '',
+ #       'HOST': 'localhost',
+ #       'PORT': '3306',
+  #      'OPTIONS': {
+   #         'sql_mode': 'traditional',
+  #      }
+ #   }
+#}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'plagos',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
