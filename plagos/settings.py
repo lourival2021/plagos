e95957f329d88a6cb684c01453d2d11d7969b5f4
Lourival da Silva Junior <82545482+lourival2021@users.noreply.github.com>
2023-05-31 21:47:00 -0300

    Update settings.py

--- plagos/settings.py
+++ plagos/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-#ALLOWED_HOSTS = ['18.222.20.202']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.222.20.202']
+#ALLOWED_HOSTS = []
 
 # Application definition
 
